9f59a7430078253cd7eec97886f491c898691283
Adrian Josele Quional <13906452+ajgquional@users.noreply.github.com>
2023-07-29 10:17:46 +0800

    Update Enemy.cs
    Added a code to "catch" NullReferenceException error. Specifically, a condition is added to check if it's really the player who touches the enemy.

--- Enemy.cs
+++ Enemy.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    // enemy properties:
-    public float speed;             // enemy speed
+    // public field specifying enemy properties:
+    public float speed = 2;         // enemy speed
     public Transform target;        // target that the enemy should move toward
     public int playerDamage = 2;    // damage to be inflicted by the enemy
 
@@ -22,10 +22,15 @@ public class Enemy : MonoBehaviour
     // when the enemy collides with the player, the player attains damage 
     private void OnTriggerEnter(Collider other)
     {
-        // getting the Player component of the designated player in the game
-        Player player = other.GetComponent<Player>();
+        // condition to ensure that it is really the player who touched the enemy
+        // primarily to "catch" NullReferenceException error
+        if (other.tag == "Player")
+        {
+            // getting the Player component of the designated player in the game
+            Player player = other.GetComponent<Player>();
 
-        // player takes damage by calling the TakeDamage method of the player
-        player.TakeDamage(playerDamage);
+            // player takes damage by calling the TakeDamage method of the player
+            player.TakeDamage(playerDamage);
+        }
     }
 }
