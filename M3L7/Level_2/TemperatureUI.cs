10785fac6ca7366c5d9b16e1df3491219cd12b89
Adrian Josele Quional <13906452+ajgquional@users.noreply.github.com>
2026-01-25 11:45:30 +0800

    Update TemperatureUI.cs

--- M3L7/Level_2/TemperatureUI.cs
+++ M3L7/Level_2/TemperatureUI.cs
@@ -2,12 +2,13 @@
  * Author: 
  *      Adrian Josele G. Quional
  * 
- * Program description: 
- *      This program implements an update on the Player's
- *      temperature on the UI.
+ * Description: 
+ *      Updates the Player's temperature on the UI.
  * 
- * How to use the script:
+ * How to use:
  *      - Attach the script to the Player in Level 2
+ *      - Link the Temperature instance of the Player
+ *      - Link the temperature text
  * **************************************************/
 
 // libraries
@@ -15,18 +16,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using TMPro;	// library for working with TextMeshPro
- 
+using TMPro;    // library for working with TextMeshPro
+
 public class TemperatureUI : MonoBehaviour
 {
-	public Temperature temperature;			// refers to the instance of the Player's temp class
-	public TextMeshProUGUI temperatureText;	// refers to object containing the TextMeshPro component
+    public Temperature temperature;         // refers to the instance of the Player's temp class
+    public TextMeshProUGUI temperatureText; // refers to object containing the TextMeshPro component
 
-	// continuously update the temperature text in the UI, every frame
-	void Update()
-	{
-		// rounds off the temperature before assigning it to the text property of the temperatureText
-		float roundTemperature = Mathf.Round(temperature.temperatureCurrent * 10.0f) * 0.1f;
-		temperatureText.text = roundTemperature.ToString();
-	}
+    // continuously update the temperature text in the UI, every frame
+    void Update()
+    {
+        // rounds off the temperature before assigning it to the text property of the temperatureText
+        float roundTemperature = Mathf.Round(temperature.temperatureCurrent * 10.0f) * 0.1f;
+        temperatureText.text = roundTemperature.ToString();
+    }
 }
